ab5b8f2a94bb67a932a82f9e8d356de5fe07bcf2
Memonia <51869800+Memonia@users.noreply.github.com>
2024-06-01 11:30:05 +0400

    [skip actions] Forgot to cleanup the 'Sandbox'

--- test/Sandbox/Program.cs
+++ test/Sandbox/Program.cs
@@ -1,51 +1,10 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-
-using Barbados.StorageEngine;
-using Barbados.StorageEngine.Documents;
-
-namespace Sandbox
+﻿namespace Sandbox
 {
 	internal sealed class Program
 	{
 		static void Main(string[] args)
 		{
-			File.Delete("barbados.db");
-
-			// Our database file is called 'barbados.db'
-			using var context = new BarbadosContext("barbados.db", openOrCreate: true);
-
-			// Create a collection
-			context.BarbadosController.CreateCollection("users");
-			context.BarbadosController.CreateIndex("users", "name");
-			var collection = context.BarbadosController.GetCollection("users");
-
-			// Builder is used to create documents
-			var documentBuilder = new BarbadosDocument.Builder();
-
-			// Create a few user documents
-			var user1FavGame = documentBuilder
-				.Add("name", "Genshin " + Enumerable.Repeat("a", 64 * 64))
-				.Add("review", "Fix Mona!")
-				.Add("reviewScore", (byte)20)
-				.Add("hoursPlayed", 17)
-				.Build();
-
-			var c = 4096;
-			for (int i = 0; i < c; ++i)
-			{
-				documentBuilder.Add(i.ToString(), user1FavGame);
-			}
-
-			var user = documentBuilder.Build();
-			var count = 1024;
-			for (int i = 0; i < count; ++i)
-			{
-				collection.Insert(user);
-			}
-
-			Console.WriteLine(collection.GetCursor().ToList().Count); 
+			// Remove everything before commit
 		}
 	}
 }
