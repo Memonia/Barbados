17db6a8b6ce07647ed326312449e8907339b2322
Memonia <51869800+Memonia@users.noreply.github.com>
2024-12-04 00:35:23 +0400

    Rewrite hook script in powershell. Bash script fails depending on where it's run from for unknown reasons

--- hooks/check-sandbox.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sandbox_path='test/Sandbox'
-if git diff --cached --name-only | grep -q "$sandbox_path"; then
-	echo -e \
-	    "Detected changes in '${sandbox_path}'. If you meant to commit them, set 'SKIP' to 'check-sandbox'\n" \
-		"bash: SKIP='check-sandbox'\n" \
-		"pwsh: \$env:SKIP='check-sandbox'" 
-	exit 1
-fi
